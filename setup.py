13124212d2ecfd5ddc69f4965fdd9a7dab788220
Matthias Osswald <m@osswald.li>
2019-10-18 09:49:06 +0200

    Add changelog entry, test.cfg and classifier.

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(name='pas.plugins.trustedproxyauth',
       classifiers=[
         'Framework :: Plone',
         'Framework :: Plone :: 4.2',
+        'Framework :: Plone :: 5.1',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ],
